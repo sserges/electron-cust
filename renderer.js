169a34fe5d0efd59af47ad2eba5a1d4b1a328aa7
sserges <agounkeserge@gmail.com>
2018-11-22 10:37:03 +0000

    Hello world app completed

--- renderer.js
+++ renderer.js
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var fs = require('fs')
+fs.readdir(__dirname + '/data/', function(err, files) {
+	if (err !== null) {
+		console.log(err)
+		return
+	}
+
+	files.forEach(function(file) {
+		$('#data').append($('<li>').text(file))
+	})
+})
